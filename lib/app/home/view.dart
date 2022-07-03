8aee176acaa500e5a9dfc58e298736ed45af3cc2
arvindsangwan <rvndsngwn@gmail.com>
2022-07-03 23:14:57 +0530

    info text added

--- lib/app/home/view.dart
+++ lib/app/home/view.dart
@@ -8,18 +8,24 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: ElevatedButton(
-        onPressed: () {
-          context.push(
-              "/over-view?title=Over View Page&subtitle=This is dynamic data.&imageUrl=https://picsum.photos/id/1/200/300");
-        },
-        child: Text(
-          "Click me".toUpperCase(),
-          textScaleFactor: 3,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              context.push(
+                  "/over-view?title=Over View Page&subtitle=This is dynamic data.&imageUrl=https://picsum.photos/id/1/200/300");
+            },
+            child: Text(
+              "Click me".toUpperCase(),
+              textScaleFactor: 3,
+            ),
+          ),
         ),
-      ),
+        const Text(
+            "Title: Over View Page,\nSubtitle: This is dynamic data.\nImageUrl: https://picsum.photos/id/1/200/300"),
+      ],
     );
   }
 }
